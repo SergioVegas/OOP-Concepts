b176f2a356a5e842d5781fd66cade2b1da585697
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-01-09 19:56:43 +0100

    Error con herencia

---
+++ oopconcepts/Cactus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+public class Cactus : Plant
+{
+	private int spikes { get; set; }
+    private int petals { get; set; }
+
+
+	public Cactus( int spikes, int petals) : base (colour, surface, height)
+	{
+		this.spikes = spikes;
+		this.petals = petals;
+		
+	}
+    public override bool MakeChlorophyll( string surface )
+	{
+        return surface == "HalfWet" ? true : false;
+    }
+}
---
+++ oopconcepts/Plant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Plant
+{
+    private string colour;
+    private string surface;
+    private double height;
+
+	//Setters
+	public  virtual void SetColour(string colour) {  this.colour = colour; }
+	public virtual void SetSurface(string surface) { this.surface = surface; }
+	public virtual void SetHeight(double height) { this.height = height; }
+
+	//Getters
+	public virtual string GetColour() { return this.colour; }
+	public virtual string GetSurface() { return this.surface; }
+	public virtual double GetHeight() { return this.height; }
+
+	//Constructor
+	public Plant( string colour, string surface, double height)
+	{
+		this.SetColour(colour);
+		this.SetSurface(surface);
+		this.SetHeight(height);
+	}
+    //Others Methods
+    public virtual bool MakeChlorophyll (string surface)
+	{
+
+		return surface == "Wet" ? true : false ;
+    }
+
+}
